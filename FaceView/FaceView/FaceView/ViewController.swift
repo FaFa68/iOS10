cfe8a9a0a652c27cfeef02e38810def28cd0d0a2
FaFa <farshad.faradji@gmail.com>
2017-03-07 14:16:58 +0100

    Project FaceView created and added.

---
+++ FaceView/FaceView/FaceView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FaceView
+//
+//  Created by Farshad Faradji on 07/03/17.
+//  Copyright © 2017 FaFa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
