c9cec3811450e593c98528135897a54c2fdb386b
FaFa <farshad.faradji@gmail.com>
2017-03-08 12:58:37 +0100

    FaceViewController compeleted

--- FaceView/FaceView/FaceView/FaceViewController.swift
+++ FaceView/FaceView/FaceView/FaceViewController.swift
@@ -9,17 +9,34 @@
 import UIKit
 
 class FaceViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    var expression = FacialExpression(eyes: .Open, eyeBrows: .Normal , mouth: .Smile) {
+        didSet {
+            updateUI()
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBOutlet weak var faceView: FaceView! {
+        didSet {
+            updateUI()
+        }
     }
-
-
+    private var mouthCurvatures = [FacialExpression.Mouth.Frown     : -1.0,
+                                   FacialExpression.Mouth.Smirk     : -0.5,
+                                   FacialExpression.Mouth.Neutral   :  0.0,
+                                   FacialExpression.Mouth.Grin      :  0.5,
+                                   FacialExpression.Mouth.Smile     :  1.0]
+    private var eyeBrowTilits = [FacialExpression.EyeBrows.Furrowed : -0.5,
+                                 FacialExpression.EyeBrows.Normal   :  0.0,
+                                 FacialExpression.EyeBrows.Relaxed  :  0.5]
+    private func updateUI() {
+        switch expression.eyes {
+        case .Open: faceView.eyesOpen   = true
+        case .Closed: faceView.eyesOpen = false
+        case .Squinting: faceView.eyesOpen = false
+        }
+        faceView.mouthCurvature = mouthCurvatures[expression.mouth] ?? 0.0
+        faceView.eyeBrowTilt = eyeBrowTilits[expression.eyeBrows]   ?? 0.0
+    }
 }
 
